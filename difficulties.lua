3c41ab7da8a5ba64d18996e3b6c6cbf984400ab2
Inpace <13023526+inpaceus@users.noreply.github.com>
2020-03-14 11:35:36 -0500

    Log world difficulty change to console
    If someone changes the world difficulty, it will be logged.

--- difficulties.lua
+++ difficulties.lua
@@ -33,17 +33,18 @@ local IsEntityBlockedInPeaceful =
 function HandleDifficultyCommand ( Split, Player )
 	if (Split[2] == nil) then
 		if (#Split == 1) then
-			SendMessage(Player, "Current world difficulty: " .. GetWorldDifficulty(Player:GetWorld()))
-			SendMessage(Player, "To change: " .. Split[1] .. " <peaceful|easy|normal|hard>")
+			SendMessage(Player, cChatColor.LightGray .. "Current world difficulty: " .. GetWorldDifficulty(Player:GetWorld()))
+			SendMessage(Player, cChatColor.LightGray .. "To change: " .. Split[1] .. " <peaceful|easy|normal|hard>")
 		else
-			SendMessage( Player, "Usage: " .. Split[1] .. " <peaceful|easy|normal|hard>" )
+			SendMessage( Player, cChatColor.LightGray .. "Usage: " .. Split[1] .. " <peaceful|easy|normal|hard>" )
 		end
 		return true
 	end
 
 	if (Split[2] == "peaceful") or (Split[2] == "0") or (Split[2] == "p") then
 		SetWorldDifficulty(Player:GetWorld(), 0)
-		SendMessage( Player, "World difficulty set to peaceful" )
+		LOGINFO(Player:GetName() .. " set the world difficulty to peaceful.")
+		SendMessage(Player, cChatColor.LightGray .. "Set the world difficulty to peaceful.")
 
 		--Remove mobs which are not allowed in peaceful
 		Player:GetWorld():ForEachEntity(
@@ -58,15 +59,18 @@ function HandleDifficultyCommand ( Split, Player )
 		)
 	elseif (Split[2] == "easy") or (Split[2] == "1") or (Split[2] == "e") then
 		SetWorldDifficulty(Player:GetWorld(), 1)
-		SendMessage( Player, "World difficulty set to easy" )
+		LOGINFO(Player:GetName() .. " set the world difficulty to easy.")
+		SendMessage(Player, cChatColor.LightGray .. "Set the world difficulty to easy.")
 	elseif (Split[2] == "normal") or (Split[2] == "2") or (Split[2] == "n") then
 		SetWorldDifficulty(Player:GetWorld(), 2)
-		SendMessage( Player, "World difficulty set to normal" )
+		LOGINFO(Player:GetName() .. " set the world difficulty to normal.")
+		SendMessage(Player, cChatColor.LightGray .. "Set the world difficulty to normal.")
 	elseif (Split[2] == "hard") or (Split[2] == "3") or (Split[2] == "h") then
 		SetWorldDifficulty(Player:GetWorld(), 3)
-		SendMessage( Player, "World difficulty set to hard" )
+		LOGINFO(Player:GetName() .. " set the world difficulty to hard.")
+		SendMessage(Player, cChatColor.LightGray .. "Set the world difficulty to hard.")
 	else
-		SendMessage( Player, "Usage: " .. Split[1] .. " <peaceful|easy|normal|hard>" )
+		SendMessage(Player, cChatColor.LightGray .. "Usage: " .. Split[1] .. " <peaceful|easy|normal|hard>")
 	end
 
 	return true
