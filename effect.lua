543a6ba4dc522331d64df9ee89a8d30f5113ae5e
Inpace <13023526+inpaceus@users.noreply.github.com>
2020-03-14 18:06:17 -0500

    Clean up syntax and log global effect changes
    Global changes in effects should be logged to console.  Cleaned up syntax for usage and response messages.

--- effect.lua
+++ effect.lua
@@ -11,51 +11,53 @@ function HandleEffectCommand(Split, Player)
 		local Seconds = tonumber(Split[4]) * 20
 		OtherPlayer:AddEntityEffect(EffectID, Seconds, Amplifier)
 		if Split[2] ~= "@a" then
-			Player:SendMessageSuccess("Successfully added effect to player \"" .. OtherPlayer:GetName() .. "\" for " .. Split[4] .. " seconds")
+			Player:SendMessageSuccess(cChatColor.LightGray .. "Added effect to " .. OtherPlayer:GetName() .. " for " .. Split[4] .. " seconds.")
 		end
 	end
 	local RemoveEffects = function(OtherPlayer)
 		OtherPlayer:ClearEntityEffects()
 		if Split[2] ~= "@a" then
-			Player:SendMessageSuccess("Successfully removed effects from player \"" .. OtherPlayer:GetName() .. "\"")
+			Player:SendMessageSuccess(cChatColor.LightGray .. "Removed effects from " .. OtherPlayer:GetName() .. ".")
 		end
 	end
 	if Split[3] ~= "clear" then
 		if Split[3] == nil then
-			Player:SendMessageInfo("Usage: " .. Split[1] .. " <player> <effect> [seconds] [amplifier] OR " .. Split[1] .. " <player> clear")
+			Player:SendMessageInfo(cChatColor.LightGray .. "Usage: " .. Split[1] .. " <player> <effect> [seconds] [amplifier] | " .. Split[1] .. " <player> clear")
 		elseif EffectID == nil or EffectID < 1 or EffectID > 23 then
-			Player:SendMessageFailure("Invalid effect ID \"" .. Split[3] .. "\"")
+			Player:SendMessageFailure(cChatColor.LightGray .. "Invalid effect: " .. Split[3])
 		elseif tonumber(Split[4]) == nil then
-			Player:SendMessageFailure("Invalid duration \"" .. Split[4] .. "\"")
+			Player:SendMessageFailure(cChatColor.LightGray .. "Invalid duration: " .. Split[4])
 		elseif tonumber(Split[4]) < 0 then
-			Player:SendMessageFailure("The duration in seconds must be at least 0")
+			Player:SendMessageFailure(cChatColor.LightGray .. "Duration in seconds must be a positive integer.")
 		elseif tonumber(Split[4]) > 1000000 then
-			Player:SendMessageFailure("The duration in seconds must be at most 1000000")
+			Player:SendMessageFailure(cChatColor.LightGray .. "Duration in seconds must not exceed one million.")
 		elseif Amplifier == nil then
-			Player:SendMessageFailure("Invalid amplification amount \"" .. Split[5] .. "\"")
+			Player:SendMessageFailure(cChatColor.LightGray .. "Invalid amplification: " .. Split[5] .. ".")
 		elseif Amplifier < 0 then
-			Player:SendMessageFailure("The amplification amount must be at least 0")
+			Player:SendMessageFailure(cChatColor.LightGray .. "Amplification must be a positive integer.")
 		elseif Amplifier > 24 and EffectID == 19 or Amplifier > 24 and EffectID == 20 then
-			Player:SendMessageFailure("The amplification amount must be at most 24")
+			Player:SendMessageFailure(cChatColor.LightGray .. "Amplification must not exceed twenty-four.")
 		elseif Amplifier > 49 and EffectID == 10 then
-			Player:SendMessageFailure("The amplification amount must be at most 49")
+			Player:SendMessageFailure(cChatColor.LightGray .. "Amplification must not exceed forty-nine.")
 		elseif Amplifier > 255 then
-			Player:SendMessageFailure("The amplification amount must be at most 255")
+			Player:SendMessageFailure(cChatColor.LightGray .. "Amplification must not exceed two hundred and fifty-five.")
 		elseif Split[2] == "@a" then
 			cRoot:Get():ForEachPlayer(ApplyEffect)
-			Player:SendMessageSuccess("Successfully added effect to every player for " .. Split[4] .. " seconds")
+			LOGINFO(Player:GetName() .. " applied a global effect.")
+			Player:SendMessageSuccess(cChatColor.LightGray .. "Added effect to every player for " .. Split[4] .. " seconds.")
 		else
 			if not cRoot:Get():FindAndDoWithPlayer(Split[2], ApplyEffect) then
-				Player:SendMessageFailure("Player \"" .. Split[2] .. "\" not found")
+				Player:SendMessageFailure(cChatColor.LightGray .. "Couldn't find that player.")
 			end
 		end
 	else
 		if Split[2] == "@a" then
 			cRoot:Get():ForEachPlayer(RemoveEffects)
-			Player:SendMessageSuccess("Successfully removed effects from every player")
+			LOGINFO(Player:GetName() .. " globally cleared effects.")
+			Player:SendMessageSuccess(cChatColor.LightGray .. "Removed effects from every player.")
 		else
 			if not cRoot:Get():FindAndDoWithPlayer(Split[2], RemoveEffects) then
-				Player:SendMessageFailure("Player \"" .. Split[2] .. "\" not found")
+				Player:SendMessageFailure(cChatColor.LightGray .. "Couldn't find that player.")
 			end
 		end
 	end
