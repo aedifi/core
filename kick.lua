5a85fb356d8ca289d5ab61ebf6e29407085acefc
Inpace <13023526+inpaceus@users.noreply.github.com>
2020-03-14 18:24:43 -0500

    Update syntax and use rose color for kick message
    We don't need to log kicks to console because that's already provided in the Cuberite software.

--- kick.lua
+++ kick.lua
@@ -1,11 +1,11 @@
 function HandleKickCommand( Split, Player )
 
 	if( #Split < 2 ) then
-		SendMessage( Player, "Usage: " .. Split[1] .. " <player> [reason ...]" )
+		SendMessage(Player, cChatColor.LightGray .. "Usage: " .. Split[1] .. " <player> [reason]")
 		return true
 	end
 
-	local Reason = cChatColor.Red .. "You have been kicked."
+	local Reason = cChatColor.Rose .. "You have been kicked."
 	if ( #Split > 2 ) then
 		Reason = table.concat( Split, " ", 3 )
 	end
@@ -19,11 +19,11 @@ function HandleKickCommand( Split, Player )
 
 	cRoot:Get():FindAndDoWithPlayer(Split[2], Kick)
 	if (IsPlayerKicked) then
-		SendMessage( Player, "Kicked " ..Split[2] )
+		SendMessage(Player, cChatColor.LightGray .. "Kicked the player " .. Split[2] .. ".")
 		return true
 	end
 	if (IsPlayerKicked == false) then
-		SendMessageFailure( Player, "Player not found" )        
+		SendMessageFailure( Player, cChatColor.LightGray .. "Couldn't find that player.")       
 		return true
 	end
 end
