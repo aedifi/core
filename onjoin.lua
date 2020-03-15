f17310f32b6cac0f56b77696336d21ca8349ae31
Inpace <13023526+inpaceus@users.noreply.github.com>
2020-03-15 08:55:10 -0500

    Syntax for player connection
    has joined the game -> has connected.

--- onjoin.lua
+++ onjoin.lua
@@ -3,11 +3,7 @@ function OnPlayerJoined(a_Player)
 	ShowMOTDTo(a_Player)
 	
 	-- Add a message to the webadmin chat:
-	WEBLOGINFO(a_Player:GetName() .. " has joined the game")
+	WEBLOGINFO(a_Player:GetName() .. " has connected.")
 	
 	return false
 end
-
-
-
-
